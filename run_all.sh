089cd1972bf6e15300bd2ff587bbdf84ce12c0db
Robert A. Buss <m1rab03@iflx1.rsma.frb.gov>
2016-02-25 15:42:17 -0500

    Updated script to completely reset the data, and made an alternative version that simply updates everything.

--- run_all.sh
+++ run_all.sh
@@ -2,20 +2,15 @@ rm *.html
 rm *.pyc
 
 echo "Parsing Optionmetrics Data"
-rm ~/data/optionmetrics/temp/*temp
 python parse_optionmetrics.py
 
 echo ""
 echo "Cleaning Weight Data"
-rm ~/data/impliedCorr/FTSE_weights_from_pdf.csv
 yes "" | python GetFTSEWeights.py
-rm ~/data/impliedCorr/STOXX_weights_from_pdf.csv
 yes "" | python GetSTOXXWeights.py
 
 echo ""
 echo "Joining Optionmetrics and Weight Data"
-rm ~/data/impliedCorr/STOXXCombinedData.csv
-rm ~/data/impliedCorr/FTSECombinedData.csv
 python add_weights.py
 
 echo ""
